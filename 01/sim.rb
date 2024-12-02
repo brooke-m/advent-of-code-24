031b0af478bc4a146e2f751a32a8ccf72e057f76
brooke <brooke.meleah@gmail.com>
2024-12-02 13:46:46 +1300

    the cooler two stars

--- 01/sim.rb
+++ 01/sim.rb
@@ -7,15 +7,15 @@ left_input = []
 right_input = []
 similarity = 0;
 
-input_lines = b = IO.readlines("input.txt", chomp: true)
+input_lines = IO.readlines("input.txt", chomp: true)
 
-input_lines.each_with_index do |v, i|
+input_lines.each do |v|
   l, r = v.split()
   left_input << l
   right_input << r
 end
 
-left_input.each_with_index do |v, i|
+left_input.each do |v|
   similarity += (v.to_i * right_input.count(v))
 end
 
